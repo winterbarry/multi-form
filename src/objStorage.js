0c8cc587f734cf2606c6159a186013082b38c99b
Obe Ajilima <ajilimaobe@gmail.com>
2026-05-08 12:36:55 +0100

    added logic to store form 2 data

--- src/objStorage.js
+++ src/objStorage.js
@@ -1,6 +1,7 @@
-// store all form data
+// variable to store all form data
 const formDataAll = {};
 
+// to store form 1 inputs
 export function setStepData(step, data) {
     formDataAll[step] = data;
 }
@@ -8,3 +9,19 @@ export function setStepData(step, data) {
 export function getFormData() {
   return formDataAll;
 }
+
+// to store form 2 inputs
+const paymentPlanInput = document.querySelectorAll('input[name="plan"]') // select all radio buttons 
+
+// loop through and attach event listeners to radio buttons
+paymentPlanInput.forEach((input) => {
+  // store the selected plan
+  input.addEventListener("change", () => { 
+    formDataAll.plan = {
+      selectedPlan: input.value,
+      billingType: input.value.includes("monthly") ? "monthly" : "yearly",
+    }
+    
+    console.log(formDataAll);
+  });
+});
